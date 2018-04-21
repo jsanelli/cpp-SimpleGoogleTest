9d60fc93a1700729ff120029e39eb9074e1f5830
John <john@24plug.com>
2018-04-21 12:10:31 -0400

    add class

---
+++ src/speak.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "../include/speak.h"
+
+Speak :: Speak() {
+  i_ = 2;
+}
+
+Speak :: Speak(int i) {
+  i_ = i;
+}
+
+Speak :: ~Speak() {
+}
+
+int Speak :: GetI() {
+  return i_;
+}
+
+void Speak :: SaySomething() {
+  std::cout << "Hello Class, your number is " << GetI() << std::endl;
+}
