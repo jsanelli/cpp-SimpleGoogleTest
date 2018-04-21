7e3200b889554f5c2fa9013fee74095b7a82a4c7
John <john@24plug.com>
2018-04-20 22:10:09 -0400

    +get_char

---
+++ src/get_char.cc
@@ -0,0 +1,4 @@
+char GetChar() { 
+  char retval = 123;
+  return retval;
+}
--- test/test.cc
+++ test/test.cc
@@ -1,22 +1,25 @@
-#include "../src/sqrt.cc"
 #include <gtest/gtest.h>
+
+#include "../src/sqrt.cc"
+#include "../src/get_char.cc"
  
-TEST(HelloWorldTestNumberOne, PositiveNos) 
-    { 
-    ASSERT_EQ(66, squareRoot(36.0));
-//  ASSERT_EQ(18.0, squareRoot(324.0));
-//  ASSERT_EQ(25.4, squareRoot(645.16));
-//  ASSERT_EQ(0, squareRoot(0.0));
-    }
-/*
-TEST(SquareRootTest, NegativeNos) 
-    {
-    ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2));
-    }
-*/ 
-int main(int argc, char **argv) 
-    {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-    }
+TEST(HelloWorldTestNumberOne, SubTestOne) {
+  ASSERT_EQ(6, squareRoot(36.0));
+}
+
+TEST(HelloWorldTestNumberOne, SubTestTwo) {
+  ASSERT_EQ(6, squareRoot(36.0));
+}
+
+TEST(HelloWorldTestNumberTwo, SubTestOne) {
+  ASSERT_EQ(6, squareRoot(36.0));
+}
+
+TEST(GetChar, SubTest1) {
+  ASSERT_EQ(123, GetChar());
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
