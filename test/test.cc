9948a81b768657ec21f04a1a88cb9cb485238963
John <john@24plug.com>
2018-04-21 15:33:26 -0400

    add private function tests

--- test/test.cc
+++ test/test.cc
@@ -6,22 +6,13 @@
 #include "../src/speak.cc"
 #include "../include/speak.h"
  
-TEST(GetChar, SubTest1) {
+TEST(GetNumber, SubTest1) {
   auto min = 0;
   auto count = 300;
-  auto x = 0, y = 0;
-
-  Speak alice;
-  TestSpeak testspeak;
 
   for (auto i=min; i<count; i++) {
     //test GetChar
     ASSERT_EQ(i, GetChar(i));
-
-    // test public functions
-    ASSERT_TRUE(alice.Compare(x, y));
-    // test private functions
-    ASSERT_TRUE(testspeak.GetConclusion());  
   }
 }
 
@@ -35,4 +26,19 @@ int main(int argc, char **argv) {
   ::testing::GTEST_FLAG(output) = "xml:results.xml";
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+ 
+TEST(Speak, SubTest1) {
+  auto min = 0;
+  auto count = 300;
+
+  Speak alice;
+  TestSpeak testspeak;
+
+  for (auto i=min; i<count; i++) {
+    // test public functions
+    ASSERT_TRUE(alice.Compare(i, i));
+    // test private functions
+    ASSERT_TRUE(testspeak.GetConclusion());  
+  }
 }
